bc4c5cb297a44a2c54f4efd07a1edd24fb41887e
Eva Israelian <evais1@mail.ru>
2022-10-01 18:45:58 +0300

    add bearer token auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  before_action :check_bearer_token
+
+  private
+  
+  def check_bearer_token
+    token = request.headers['Authorization']&.split&.last
+
+    unless token == AUTH_TOKEN
+      render json: { error: 'Auth token invalid' }, status: :forbidden
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,7 @@ module CurrencyExchanger
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    ::AUTH_TOKEN = ENV.fetch('AUTH_TOKEN').freeze
   end
 end
